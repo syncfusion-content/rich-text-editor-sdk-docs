657cd5d43456378f1a87eab3db3600b8bcd77c47
gugaprasad <gugaprasad.natarajan@syncfusion.com>
2026-08-12 13:50:06 +0530

    Task(1046945): Resolved the reverted code snippet changes

--- code-snippet/rich-text-editor-sdk/typescript/rich-text-editor/file-browser-cs2/index.ts
+++ code-snippet/rich-text-editor-sdk/typescript/rich-text-editor/file-browser-cs2/index.ts
@@ -3,17 +3,16 @@
 import { RichTextEditor, HtmlEditor, Toolbar, QuickToolbar, Image, FileManager } from '@syncfusion/ej2-richtexteditor';
 RichTextEditor.Inject(HtmlEditor, Toolbar, QuickToolbar, Image, FileManager);
 
-let hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
+let hostUrl: string = 'https://services.syncfusion.com/js/production/';
 
 let editor: RichTextEditor = new RichTextEditor({
     fileManagerSettings: {
         enable: true,
-        path: '/Pictures/Food',
         ajaxSettings: {
-            url: hostUrl + 'api/FileManager/FileOperations',
-            getImageUrl: hostUrl + 'api/FileManager/GetImage',
-            uploadUrl: hostUrl + 'api/FileManager/Upload',
-            downloadUrl: hostUrl + 'api/FileManager/Download'
+            url: hostUrl + 'api/RichTextEditor/FileOperations',
+			getImageUrl: hostUrl + 'api/RichTextEditor/GetImage',
+			uploadUrl: hostUrl + 'api/RichTextEditor/Upload',
+			downloadUrl: hostUrl + 'api/RichTextEditor/Download'
         }
     },
     toolbarSettings: {
