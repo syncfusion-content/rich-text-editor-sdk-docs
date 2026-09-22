b6ca3496d9796f62f972557074a77fd9ac683945
Aravind <aravind.prabhakaran@syncfusion.com>
2026-09-22 18:33:26 +0530

    1056282: Added the getting started in the vue modern rich text editor

---
+++ code-snippet/rich-text-editor-sdk/vue/richtexteditor-ui/getting-started/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <ejs-richtexteditor-ui placeholder="Type something ..."></ejs-richtexteditor-ui>
+</template>
+
+<script>
+import { RichTextEditorUIComponent } from '@syncfusion/ej2-vue-richtexteditor-ui';
+
+export default {
+  name: 'App',
+  components: {
+    'ejs-richtexteditor-ui': RichTextEditorUIComponent
+  }
+};
+</script>
+
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-lists/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-popups/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-buttons/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-navigations/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-splitbuttons/styles/tailwind3.css";
+@import "../../node_modules/@syncfusion/ej2-vue-richtexteditor-ui/styles/tailwind3.css";
+</style>
